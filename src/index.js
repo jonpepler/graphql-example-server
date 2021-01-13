512c24ae12aff0b6c82cb6472063fa560b12773a
Jon Pepler <jonathan.pepler@gmail.com>
2021-01-13 14:37:28 +0000

    Return a list of links on feed query

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import { ApolloServer } from 'apollo-server'
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Mutation {
+    post(url: String!, description: String!): Link!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
+const links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
 
 const resolvers = {
   Query: {
-    info: () => 'This is the API of a Hackernews Clone'
+    info: () => 'This is the API of a Hackernews Clone',
+    feed: () => links
   }
 }
 
