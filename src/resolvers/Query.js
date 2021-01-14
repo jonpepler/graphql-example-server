1ed1003c4a2021c4463a6ba6fa4e3699308f3912
Jon Pepler <jonathan.pepler@gmail.com>
2021-01-14 17:03:26 +0000

    Support filtering link feed

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,6 +2,15 @@ import { findLinkByID } from './helpers.js'
 
 export const Query = {
   info: () => 'This is the API of a Hackernews Clone',
-  feed: async (parent, args, { prisma }) => prisma.link.findMany(),
+  feed: async (parent, { filter }, { prisma }) => prisma.link.findMany(filter
+    ? {
+        where: {
+          OR: [
+            { url: { contains: filter } },
+            { description: { contains: filter } }
+          ]
+        }
+      }
+    : {}),
   link: (_, args, { prisma }) => findLinkByID(args.id, prisma)
 }
