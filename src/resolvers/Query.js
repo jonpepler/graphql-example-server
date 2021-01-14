cd17578de79c76ebe3aa274a15ed6c49eb40a560
Jon Pepler <jonathan.pepler@gmail.com>
2021-01-14 17:22:26 +0000

    Support count field on feed

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,15 +2,21 @@ import { findLinkByID } from './helpers.js'
 
 export const Query = {
   info: () => 'This is the API of a Hackernews Clone',
-  feed: async (parent, { filter }, { prisma }) => prisma.link.findMany(filter
-    ? {
-        where: {
-          OR: [
-            { url: { contains: filter } },
-            { description: { contains: filter } }
-          ]
+  feed: async (parent, { filter }, { prisma }) => {
+    const links = await prisma.link.findMany(filter
+      ? {
+          where: {
+            OR: [
+              { url: { contains: filter } },
+              { description: { contains: filter } }
+            ]
+          }
         }
-      }
-    : {}),
+      : {})
+    return {
+      links,
+      count: links.length
+    }
+  },
   link: (_, args, { prisma }) => findLinkByID(args.id, prisma)
 }
